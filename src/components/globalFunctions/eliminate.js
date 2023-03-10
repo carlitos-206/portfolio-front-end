3b0b4aeb776684fe0e9bf3da2f2a5ca2027c94d2
carlitos-206 <carloscaceres041@gmail,com>
2023-03-10 02:23:06 -0800

    webSQL requires Node

--- src/components/globalFunctions/eliminate.js
+++ src/components/globalFunctions/eliminate.js
@@ -1,5 +1,14 @@
-export default function eliminatePrivileges(){
+const webSQL = require('websql')
+
+export function eliminatePrivileges(){
   localStorage.setItem('blocked', 'true')
   sessionStorage.setItem('blocked', 'true')
   document.cookie = "blocked=True; expires=Fri, 01 Jan 2049 00:00:00 UTC;"
+
+
+// // webSQL not possible in react.js must use nodeJS api
+//   // creates the db
+//               //  DB name   version  description    memory alocation 
+//   let db = webSQL('blocked.db', '1.0', 'blocked', 5 * 1024 * 1024 )
+
 }
